9265d66507cb85658cba3176f7b3fb8c11ce473c
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-10 20:38:00 +0300

    Request-response check, thunderClient added

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.set("view engine", "ejs");
 
 //static file miidleware
 app.use(express.static("public"));
+app.use(express.json());
 
 //routes
 app.use("/", pageRoute);
--- routes/photoRoutes.js
+++ routes/photoRoutes.js
@@ -3,6 +3,7 @@ import * as photoController from "../controllers/photoController.js";
 
 const router = express.Router();
 
-router.route("/").get(photoController.createPhoto);
+router.route("/").post(photoController.createPhoto);
+router.route("/").get(photoController.getAllPhotos);
 
 export default router;
