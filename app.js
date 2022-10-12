8b280ea53f8c2ecd7ac5dbfa20e9957b479b5bf6
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-12 22:49:05 +0300

    Register Page, Form Processors, User Model
    -- Register Page added (register.ejs)
    -- Form processors (post method added)
    -- user model created (User)

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import conn from "./db.js";
 import pageRoute from "./routes/pageRoutes.js";
 import photoRoute from "./routes/photoRoutes.js";
+import userRoute from "./routes/userRoute.js";
 
 dotenv.config();
 const app = express();
@@ -17,11 +18,13 @@ app.set("view engine", "ejs");
 //static file miidleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //routes
 app.use("/", pageRoute);
 app.use("about", pageRoute);
 app.use("/photos", photoRoute);
+app.use("/users", userRoute);
 
 app.listen(port, () => {
   console.log(`Application running ${port} on port`);
