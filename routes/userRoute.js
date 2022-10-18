1c55f75a0407fd9bfeb237b86816f131180cade1
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-18 13:36:29 +0300

    image upload, clodinary platform, expressfileupload 2

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,9 +5,11 @@ import * as authMiddleware from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 router.route("/register").post(userController.createUser);
-router.route("/login").post(userController.loginUser);
 router
-  .route("/dashboard")
-  .get(authMiddleware.authenticateToken, userController.getDashboardPage);
+  .route("/")
+  .get(authMiddleware.authenticateToken, userController.getAllUsers);
+router
+  .route("/:id")
+  .get(authMiddleware.authenticateToken, userController.getAUser);
 
 export default router;
